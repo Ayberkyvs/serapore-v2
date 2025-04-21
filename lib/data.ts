79ee0c6193d155a097b6ae98c7cc65dd4174da4f
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-04-21 16:34:20 +0300

    chore(lib): add menu items to data.ts

---
+++ lib/data.ts
@@ -0,0 +1,34 @@
+const MenuItems: MenuItem[] = [
+	{
+		path: "/aboutus",
+		name: "About Us",
+		type: "link",
+		icon: null,
+	},
+	{
+		path: "/services",
+		name: "Services",
+		type: "link",
+		icon: null,
+	},
+	{
+		path: "/Products",
+		name: "Products",
+		type: "link",
+		icon: null,
+	},
+	{
+		path: "/gallery",
+		name: "Gallery",
+		type: "link",
+		icon: null,
+	},
+	{
+		path: "/contactus",
+		name: "Contact Us",
+		type: "button",
+		icon: null,
+	},
+];
+
+export default MenuItems;
