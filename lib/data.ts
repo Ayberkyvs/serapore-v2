bebf7cd4412cf7eea64e2b99b1ade36875d5d049
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-04-23 01:35:50 +0300

    chore(lib): :recycle: refactor data.ts, add partner logos list

--- lib/data.ts
+++ lib/data.ts
@@ -1,34 +1,14 @@
-const MenuItems: MenuItem[] = [
-	{
-		path: "/aboutus",
-		name: "About Us",
-		type: "link",
-		icon: null,
-	},
-	{
-		path: "/services",
-		name: "Services",
-		type: "link",
-		icon: null,
-	},
-	{
-		path: "/Products",
-		name: "Products",
-		type: "link",
-		icon: null,
-	},
-	{
-		path: "/gallery",
-		name: "Gallery",
-		type: "link",
-		icon: null,
-	},
-	{
-		path: "/contactus",
-		name: "Contact Us",
-		type: "button",
-		icon: null,
-	},
+export const partnerLogos = [
+	"newarc",
+	"alvit",
+	"bien",
+	"vitra",
+	"kohler",
+	"saudi",
+	"serel",
+	"otto_bagno",
+	"cersanit",
+	"american_standart",
+	"roca",
+	"hatria",
 ];
-
-export default MenuItems;
