cc4753a408172de87c54197ca3d1d4cf51224fd5
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-04-23 01:36:32 +0300

    chore(types): :label: update and organize type definitions in types.d.ts

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,6 +1,11 @@
-type MenuItem = {
+export type MenuItem = {
 	name: string;
 	path: string;
 	type: "link" | "button";
 	icon: React.ReactNode;
 };
+export type SectionHeadingProps = {
+	title: string;
+	subtitle?: string;
+};
+export type partnerLogosProps = string[];
