feb20c18d6e3f282b453c64ee9c7bd95ca45bfe4
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-04-23 21:07:40 +0300

    feat(components): :sparkles: add HorizontalScroller component using embla-carousel

---
+++ components/HorizontalScroller.tsx
@@ -0,0 +1,25 @@
+// components/HorizontalScroller.tsx
+"use client";
+import { ReactNode, useEffect } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+
+interface Props {
+	children: ReactNode;
+}
+
+export default function HorizontalScroller({ children }: Props) {
+	const [emblaRef, emblaApi] = useEmblaCarousel({
+		dragFree: true,
+		containScroll: "trimSnaps",
+	});
+
+	useEffect(() => {
+		if (emblaApi) emblaApi.reInit();
+	}, [emblaApi]);
+
+	return (
+		<div className="overflow-hidden" ref={emblaRef}>
+			<div className="flex gap-5">{children}</div>
+		</div>
+	);
+}
