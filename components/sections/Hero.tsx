9f897d1df8a04e20b9ae8e90c259c5669620f12f
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-04-25 23:53:16 +0300

    refactor(components): :recycle: refactor i18n imports

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -7,24 +7,24 @@ import InfiniteCarouselSection from "./InfiniteCarouselSection";
 
 const Hero = () => {
 	const t = useTranslations("Hero");
-	let heroTitle = t("heroTitle").split(" ");
+	let heroTitle = t("title").split(" ");
 	const heroButtons = [
 		{
-			name: t("heroButtons.seeProducts"),
+			name: t("buttons.seeProducts"),
 			path: "/products",
 			variant: "default",
 			className:
 				"bg-accent-400 text-black hover:bg-accent-500 hover:cursor-pointer",
 		},
 		{
-			name: t("heroButtons.ourServices"),
+			name: t("buttons.ourServices"),
 			path: "/services",
 			variant: "secondary",
 			className:
 				"bg-primary-400 text-white hover:bg-primary-500 hover:cursor-pointer",
 		},
 		{
-			name: t("heroButtons.companyPresentation"),
+			name: t("buttons.companyPresentation"),
 			path: "/serapore.pdf",
 			variant: "outline",
 			className: "bg-transparent border-black hover:cursor-pointer",
@@ -40,7 +40,7 @@ const Hero = () => {
 							<Verified className="inline-block text-primary-400 size-[1em] mx-[0.2em]" />
 							{heroTitle.slice(2).join(" ")}
 						</h2>
-						<p className="hero__content__description">{t("heroDescription")}</p>
+						<p className="hero__content__description">{t("description")}</p>
 						<div className="hero__content__buttons">
 							{heroButtons.map((button, index: number) => (
 								<Link href={button.path} key={index} className="size-fit">
