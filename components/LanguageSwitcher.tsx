99d0264658bbd380e682046f229219ba8a396485
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-04-26 18:51:05 +0300

    chore(components): :lipstick: add transparent background to language switcher

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import {
-	Select,
-	SelectTrigger,
-	SelectContent,
-	SelectItem,
-	SelectValue,
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
 } from "@/components/ui/select";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -13,63 +13,63 @@ import { useEffect, useState } from "react";
 const locales = ["en", "tr"];
 
 const FlagIcon = ({ lang }: { lang: string }) => {
-	const flagSrc =
-		lang === "en" ? "/media/flags/en-US.svg" : "/media/flags/tr-TR.svg";
-	return (
-		<img
-			src={flagSrc}
-			alt={`${lang} flag`}
-			className="size-4 rounded-full object-cover object-left"
-		/>
-	);
+  const flagSrc =
+    lang === "en" ? "/media/flags/en-US.svg" : "/media/flags/tr-TR.svg";
+  return (
+    <img
+      src={flagSrc}
+      alt={`${lang} flag`}
+      className="size-4 rounded-full object-cover object-left"
+    />
+  );
 };
 
 const LanguageSwitcher = () => {
-	const pathname = usePathname();
-	const router = useRouter();
+  const pathname = usePathname();
+  const router = useRouter();
 
-	const [currentLocale, setCurrentLocale] = useState("en");
+  const [currentLocale, setCurrentLocale] = useState("en");
 
-	useEffect(() => {
-		const localeFromPath = locales.find((locale) =>
-			pathname?.startsWith(`/${locale}`)
-		);
-		if (localeFromPath) {
-			setCurrentLocale(localeFromPath);
-		}
-	}, [pathname]);
+  useEffect(() => {
+    const localeFromPath = locales.find((locale) =>
+      pathname?.startsWith(`/${locale}`)
+    );
+    if (localeFromPath) {
+      setCurrentLocale(localeFromPath);
+    }
+  }, [pathname]);
 
-	const handleChange = (lang: string) => {
-		if (!pathname) return;
+  const handleChange = (lang: string) => {
+    if (!pathname) return;
 
-		// Remove existing locale from path
-		const newPath = pathname.replace(/^\/(en|tr)/, "");
-		router.push(`/${lang}${newPath === "" ? "/" : newPath}`);
-	};
+    // Remove existing locale from path
+    const newPath = pathname.replace(/^\/(en|tr)/, "");
+    router.push(`/${lang}${newPath === "" ? "/" : newPath}`);
+  };
 
-	return (
-		<div className="h-full">
-			<Select value={currentLocale} onValueChange={handleChange}>
-				<SelectTrigger className="w-fit border-0">
-					<SelectValue>
-						<FlagIcon lang={currentLocale} />
-					</SelectValue>
-				</SelectTrigger>
-				<SelectContent className="h-fit z-[99]">
-					{locales.map((locale, index: number) => (
-						<SelectItem
-							key={index}
-							value={locale}
-							className="flex items-center"
-						>
-							<FlagIcon lang={locale} />
-							<span className="uppercase small">{locale}</span>
-						</SelectItem>
-					))}
-				</SelectContent>
-			</Select>
-		</div>
-	);
+  return (
+    <div className="h-full">
+      <Select value={currentLocale} onValueChange={handleChange}>
+        <SelectTrigger className="w-fit border-0 !bg-transparent">
+          <SelectValue>
+            <FlagIcon lang={currentLocale} />
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent className="h-fit z-[99]">
+          {locales.map((locale, index: number) => (
+            <SelectItem
+              key={index}
+              value={locale}
+              className="flex items-center"
+            >
+              <FlagIcon lang={locale} />
+              <span className="uppercase small">{locale}</span>
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
 };
 
 export default LanguageSwitcher;
